355878fff5bc1426f1706d68646ca671baed178c
erdelf <erdelf.productions@gmail.com>
2017-10-25 01:40:47 +0200

    pawns reinstall more buildings without problems now

--- Source/MinifyEverything/MinifyEverything.cs
+++ Source/MinifyEverything/MinifyEverything.cs
@@ -6,6 +6,8 @@ using Verse;
 using Harmony;
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
 
 namespace MinifyEverything
 {
@@ -34,6 +36,21 @@ namespace MinifyEverything
 
             HarmonyInstance harmony = HarmonyInstance.Create("rimworld.erdelf.minify_everything");
             harmony.Patch(AccessTools.Method(typeof(Blueprint_Install), nameof(Blueprint_Install.TryReplaceWithSolidThing)), null, new HarmonyMethod(typeof(MinifyEverything), nameof(AfterInstall)));
+            harmony.Patch(AccessTools.Method(typeof(WorkGiver_ConstructDeliverResources), "InstallJob"), null, null, new HarmonyMethod(typeof(MinifyEverything), nameof(InstallJobTranspiler)));
+        }
+
+        public static IEnumerable<CodeInstruction> InstallJobTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> instructionList = instructions.ToList();
+            MethodInfo implicitConverter = AccessTools.Method(typeof(LocalTargetInfo), "op_Implicit", new Type[] { typeof(Pawn) });
+
+            for (int i = 0; i < instructionList.Count; i++)
+            {
+                CodeInstruction instruction = instructionList[i];
+                yield return instruction;
+                if (instruction.opcode == OpCodes.Call && instruction.operand == implicitConverter && instructionList[i + 1].opcode == OpCodes.Ldc_I4_1)
+                    instructionList[i + 1].opcode = OpCodes.Ldc_I4_2;
+            }
         }
 
         public static void AfterInstall(Thing createdThing)
