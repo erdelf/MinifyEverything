acd1040870d06f656dfe6ad9bd78f3d576bab7b6
erdelf <erdelf.productions@gmail.com>
2017-10-26 02:32:32 +0200

    checks for existing mini building before building (only single for now)

--- Source/MinifyEverything/MinifyEverything.cs
+++ Source/MinifyEverything/MinifyEverything.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection.Emit;
+using Verse.AI;
 
 namespace MinifyEverything
 {
@@ -37,6 +38,31 @@ namespace MinifyEverything
             HarmonyInstance harmony = HarmonyInstance.Create("rimworld.erdelf.minify_everything");
             harmony.Patch(AccessTools.Method(typeof(Blueprint_Install), nameof(Blueprint_Install.TryReplaceWithSolidThing)), null, new HarmonyMethod(typeof(MinifyEverything), nameof(AfterInstall)));
             harmony.Patch(AccessTools.Method(typeof(WorkGiver_ConstructDeliverResources), "InstallJob"), null, null, new HarmonyMethod(typeof(MinifyEverything), nameof(InstallJobTranspiler)));
+            harmony.Patch(AccessTools.Method(typeof(Designator_Build), nameof(Designator.DesignateSingleCell)), null, null, new HarmonyMethod(typeof(MinifyEverything), nameof(DesignateSingleCellTranspiler)));
+        }
+
+        public static IEnumerable<CodeInstruction> DesignateSingleCellTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            MethodInfo placeBlueprint = AccessTools.Method(typeof(GenConstruct), nameof(GenConstruct.PlaceBlueprintForBuild));
+            foreach (CodeInstruction instruction in instructions)
+                yield return (instruction.opcode == OpCodes.Call && instruction.operand == placeBlueprint) ? new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(MinifyEverything), nameof(ReplaceBlueprintForBuild))) : instruction;
+        }
+
+        static HashSet<WeakReference> usedStuff = new HashSet<WeakReference>();
+
+        public static Blueprint ReplaceBlueprintForBuild(BuildableDef sourceDef, IntVec3 center, Map map, Rot4 rotation, Faction faction, ThingDef stuff)
+        {
+            if (sourceDef is ThingDef td &&
+                td.Minifiable &&
+                map.listerThings.ThingsOfDef(td.minifiedDef).OfType<MinifiedThing>().Where(t => t.GetInnerIfMinified().Stuff == stuff).FirstOrDefault(m => map.reachability.CanReach(center, m, PathEndMode.Touch, TraverseMode.ByPawn, Danger.Deadly)) is MinifiedThing mini &&
+                !mini.IsForbidden(faction) &&
+                !map.reservationManager.IsReservedByAnyoneOf(mini, faction) && !usedStuff.Any(wr => wr.IsAlive && wr.Target == mini))
+            {
+                usedStuff.Add(new WeakReference(mini));
+                return GenConstruct.PlaceBlueprintForInstall(mini, center, map, rotation, faction);
+            }
+            else
+                return GenConstruct.PlaceBlueprintForBuild(sourceDef, center, map, rotation, faction, stuff);
         }
 
         public static IEnumerable<CodeInstruction> InstallJobTranspiler(IEnumerable<CodeInstruction> instructions)
